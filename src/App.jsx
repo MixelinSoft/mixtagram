a5b8e92cdd78ca9d8f09e035c330567e71080882
MixelinSoft <127510809+MixelinSoft@users.noreply.github.com>
2024-07-26 18:36:40 +0300

    Added Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import styles from './App.module.css';
 import Header from './components/layout/header/Header';
+import Footer from './components/layout/footer/Footer';
 
 function App() {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   );
 }
